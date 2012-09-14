4f1468c5f7fc0627e6106227629e5bc0a9031f26
Shen Junru <shenjunru@gmail.com>
2012-09-15 02:29:34 +0800

    added: Deffered qunit test.

---
+++ test/global.js
@@ -0,0 +1,6 @@
+document.write(
+    '<link href="qunit/qunit.css" rel="stylesheet">',
+    '<script src="qunit/qunit.js"></script>',
+    '<script src="../amdr.js"></script>',
+    '<div id="qunit"></div>'
+);
