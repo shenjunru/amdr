8c615dc3189ffe031de423015adc76830737323c
Shen Junru <shenjunru@gmail.com>
2012-09-18 15:06:19 +0800

    fixed: css loader for Gecko & webkit.

--- loader/css-src.js
+++ loader/css-src.js
@@ -1,3 +1,26 @@
+/*!
+ * CSS file loader for AMDR
+ * (c) 2012 Shen Junru. MIT License.
+ * http://github.com/xfsn/amdr
+ */
+
+// global imports:
+//   document {createElement(), getElementsByTagName()}
+//   devicePixelRatio
+//   opera
+
+// note:
+//   *link.onload* & *link.onerror* events.
+//   IE & Opera support 'onload' & 'onerror' correctly. others does not support.
+//   Gecko creates link.sheet right after insert to document and creates
+//   sheet.cssRules when file is loaded.
+//   Webkit creates link.sheet & sheet.cssRules when file is loaded.
+//   see: http://yearofmoo.com/2011/03/cross-browser-stylesheet-preloading/
+//
+//   *cross domain* sheet.cssRules retrieving.
+//   Gecko throws security error when file is loaded.
+//   Webkit does not throw any error, and sheet.cssRules always is null.
+
 (function(global, isIE){
     'use strict';
 
@@ -8,9 +31,10 @@
         insertPoint = firstNodeOfTagName('head') || firstNodeOfTagName('script'),
 
         // flag: supports 'onload' & 'onerror' events
-        // only IE & Opera supports actually
-        // see: http://yearofmoo.com/2011/03/cross-browser-stylesheet-preloading/
-        linkOnEvent = isIE && '[object Opera]' === ({}).toString.call(global.opera),
+        linkOnEvent = isIE || '[object Opera]' === ({}).toString.call(global.opera),
+
+        // flag: browser is based on Webkit
+        isWebkit = !!global.devicePixelRatio,
 
         // event: onload
         eventOnload = linkOnEvent ? 'onload' : 'ondone',
@@ -33,18 +57,18 @@
 
                 // events handlers
                 link[eventOnload] = function(){
-                    complete(id, link);
-                    resolve(link);
+                    complete(id, link, true);
+                    delay(resolve, link);
                 };
                 link[eventOnfail] = function(){
-                    complete(id, link, true);
-                    reject(new Error('load failures.'));
+                    complete(id, link);
+                    delay(reject, new Error('load failures.'));
                 };
                 // adds timeout id to queue
                 queue[id] = setTimeout(function(){
-                    complete(id, link, true);
-                    reject(new Error('load timeout.'));
-                }, 7000);
+                    complete(id, link);
+                    delay(reject, new Error('load timeout.'));
+                }, 7000); // TODO: get timeout config
 
                 // attributes
                 link.rel  = 'stylesheet';
@@ -66,18 +90,27 @@
         return document.getElementsByTagName(name)[0];
     }
 
-    function complete(id, link, error){
+    function delay(func, param){
+        setTimeout(function(){
+            func(param);
+        });
+    }
+
+    function complete(id, link, success){
         // stops timeout timer
         clearTimeout(queue[id]);
 
         // removed from queue
         delete queue[id];
 
+        // releases memory
+        links[id] = 1;
+
         // removed events handlers, prevents memory leaks
         link[eventOnfail] = link[eventOnload] = '';
 
         // if error occurs, remove from dom tree
-        if (error) {
+        if (!success) {
             insertPoint.removeChild(link);
         }
     }
@@ -88,16 +121,26 @@
         }
     }
 
-    function checker(id /* var */, link /* var */, rules /* var */){
+    function checker(/* var */id , link, sheet, rules, cross){
         for (id in queue) {
+            cross = isWebkit;
             link = links[id];
             // if file is loading, retrieves 'cssRules' property in firefox
             // will throws an error.
-            try { if (rules = link.sheet && link.sheet.cssRules) {
-                setTimeout(link[rules.length ? eventOnload : eventOnfail], 0);
-            } } catch(_) {}
-        }
+            if (sheet = link.sheet) {
+                try { rules = sheet.cssRules; } catch(_) {
+                    // Gecko: onload - cross domain
+                    cross = _.code === _.SECURITY_ERR;
+                }
 
+                if (rules) {
+                    link[rules.length ? eventOnload : eventOnfail]();
+                } else if (cross) {
+                    // Webkit/Gecko: onload - cross domain
+                    link[eventOnload]();
+                }
+            }
+        }
         setTimeout(checkQueue, 100);
     }
 
