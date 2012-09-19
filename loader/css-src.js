db9c00c750ca324e250695e9e7d99ee48b0c2786
Shen Junru <shenjunru@gmail.com>
2012-09-20 02:42:01 +0800

    rewrite: css loader.

--- loader/css-src.js
+++ loader/css-src.js
@@ -6,22 +6,41 @@
 
 // global imports:
 //   document {createElement(), getElementsByTagName()}
-//   devicePixelRatio
 //   opera
 
 // note:
 //   *link.onload* & *link.onerror* events.
-//   IE & Opera support 'onload' & 'onerror' correctly. others does not support.
-//   Gecko creates link.sheet right after insert to document and creates
-//   sheet.cssRules when file is loaded.
-//   Webkit creates link.sheet & sheet.cssRules when file is loaded.
+//   IE:     always fire, even 404. no 'onerror'.
+//   Opera:  only fired when request is success(2xx). no 'onerror'.
+//   Gecko:  FireFox > 9 support. other version creates 'link.sheet'
+//           right after insert to document and creates 'sheet.cssRules'
+//           when request is completed.
+//   Webkit: Chrome > 19 and Safari 6 support. other version creates
+//           'link.sheet' and 'sheet.cssRules' when request is completed.
 //   see: http://yearofmoo.com/2011/03/cross-browser-stylesheet-preloading/
 //
 //   *cross domain* sheet.cssRules retrieving.
-//   Gecko throws security error when file is loaded.
-//   Webkit does not throw any error, and sheet.cssRules always is null.
+//   IE:     only cross domain failed will throw security error.
+//   Opera:  always no sheet.cssRules property.
+//   Gecko:  throws security error when file is loaded.
+//   Webkit: no error thrown, and sheet.cssRules always is null.
+//
+//   *opera*, sets timeout as 3sec, because opera does not support 'onerror'
+//   and no other way to detect failed request. and only 'load timeout' will be
+//   thrown for failed request.
 
-(function(global, isIE){
+// browsers features:
+//        | create | insert |  200 & origin  | 200 & cross | 404 & origin | 404 & cross |
+// -------|--------|--------|----------------|-------------|--------------|-------------|
+// IE     |  sheet |    -   |                onload & rules               / rules=error |
+// -------|--------|--------|----------------|-------------|--------------|-------------|
+// Opera  |  rules |    -   | onload & rules / rules=error |              -             |
+// -------|--------|--------|----------------|-------------|--------------|-------------|
+// Gecko  |    -   |  sheet |      rules     | rules=error |    rules=0   | rules=error |
+// -------|--------|--------|----------------|-------------|--------------|-------------|
+// Webkit |    -   |    -   | sheet & rules  |                  sheet                   |
+
+(function(global, isIE, undef){
     'use strict';
 
     var // element: document
@@ -30,73 +49,110 @@
         // element: resource insert point
         insertPoint = firstNodeOfTagName('head') || firstNodeOfTagName('script'),
 
-        // flag: supports 'onload' & 'onerror' events
-        linkOnEvent = isIE || '[object Opera]' === ({}).toString.call(global.opera),
+        // flag: browser detecting
+        isWebkit = 'webkitAppearance' in document.documentElement.style,
+        isOpera  = '[object Opera]' === ({}).toString.call(global.opera),
 
-        // flag: browser is based on Webkit
-        isWebkit = !!global.devicePixelRatio,
+        // flag: skips browser features detecting
+        skipTest = isIE || isOpera,
 
-        // event: onload
-        eventOnload = linkOnEvent ? 'onload' : 'ondone',
+        // flag: waits browser features detecting
+        waitTest = !skipTest,
 
-        // event: onerror
-        eventOnfail = linkOnEvent ? 'onerror' : 'onfail',
+        // flag: IE & Opera only support 'onload' event
+        catchOnload = skipTest || undef,
+        catchOnfail = skipTest ? false : undef,
 
+        // event names
+        eventOnload = 'onload',
+        eventOnfail = 'onerror',
+
+        // collection: url maps id
         idMap = {},
+
+        // collection: link elements
         links = {},
+
+        // collection: loading timeouts
         queue = {},
-        seed  = 0;
+
+        delay = [],
+
+        // id seed
+        seed  = 0,
+
+        timer = 0;
+
+    // browser features detecting
+    // uses data url to avoid extra requests
+    skipTest || testFeature(eventOnload, 'data:text/css;base64,', function(result){
+        catchOnload = result; //            ^empty data url^
+        if (undef !== catchOnfail) {
+            testComplete();
+        }
+    });
+    skipTest || testFeature(eventOnfail, isWebkit ? 'data:' : 'data:,', function(result){
+        catchOnfail = result; //                    ^webkit^  ^gecko^
+        if (undef !== eventOnload) {
+            testComplete();
+        }
+    });
 
     define({
         load: function(name, resolve, reject){
-            var id = idMap[name] || (idMap[name] = 'amdr-css-' + seed++),
-                link;
+            var id = idMap[name] || (idMap[name] = 'amdr-css-' + seed++);
 
             if (!links[id]) {
-                links[id] = link = document.createElement('link');
-
-                // events handlers
-                link[eventOnload] = function(){
-                    complete(id, link, true);
-                    delay(resolve, link);
-                };
-                link[eventOnfail] = function(){
-                    complete(id, link);
-                    delay(reject, new Error('load failures.'));
-                };
-                // adds timeout id to queue
-                queue[id] = setTimeout(function(){
-                    complete(id, link);
-                    delay(reject, new Error('load timeout.'));
-                }, 7000); // TODO: get timeout config
-
-                // attributes
-                link.rel  = 'stylesheet';
-                link.type = 'text/css';
-                link.id   = id;
-                link.href = name;
-
-                // adds to dom tree
-                insertPoint.appendChild(link);
-
-                if (!linkOnEvent) {
-                    checkQueue();
+                if (waitTest) {
+                    // waiting for browser features detecting
+                    delay.push([id, name, resolve, reject]);
+                } else {
+                    // defines current one
+                    moduleDefine.call(global, id, name, resolve, reject);
                 }
             }
         }
     });
 
-    function firstNodeOfTagName(name){
-        return document.getElementsByTagName(name)[0];
+    function moduleDefine(id, name, resolve, reject){
+        var link = links[id] = createLink(name);
+
+        // attributes
+        link.id = id;
+
+        // events handlers
+        link[eventOnload] = function(rules){
+            // IE always fire 'onload' even 404
+            if (!skipTest || hasCssRule(link)) {
+                // load success
+                moduleComplete(id, link, true);
+                delayExecute(resolve, link);
+            } else {
+                // load failure: IE loaded with rules=0
+                link[eventOnfail]();
+            }
+        };
+        link[eventOnfail] = function(){
+            moduleComplete(id, link);
+            delayExecute(reject, new Error('load failures.'));
+        };
+        // adds timeout id to queue
+        queue[id] = setTimeout(function(){
+            moduleComplete(id, link);
+            delayExecute(reject, new Error('load timeout.'));
+        }, isOpera ? 3000 : 7000); // TODO: get timeout config
+
+        // inserts to document
+        insertPoint.appendChild(link);
+
+        // Opera & no events supported browsers
+        // uses styleSheet checking
+        if (!skipTest && !timer && (!catchOnfail || !catchOnload)) {
+            checkComplete();
+        }
     }
 
-    function delay(func, param){
-        setTimeout(function(){
-            func(param);
-        });
-    }
-
-    function complete(id, link, success){
+    function moduleComplete(id, link, success){
         // stops timeout timer
         clearTimeout(queue[id]);
 
@@ -111,37 +167,94 @@
 
         // if error occurs, remove from dom tree
         if (!success) {
-            insertPoint.removeChild(link);
+            //insertPoint.removeChild(link);
         }
     }
 
-    function checkQueue(id /* var */){
-        for (id in queue) {
-            return checker();
+    function firstNodeOfTagName(name){
+        return document.getElementsByTagName(name)[0];
+    }
+
+    function createLink(href){
+        var link = document.createElement('link');
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = href;
+        return link;
+    }
+
+    function testFeature(event, href, callback){
+        var link  = createLink(href),
+        fired = false,
+        allow = 2;
+
+        link[event] = function(){
+            fired = true;
+        };
+        insertPoint.appendChild(link);
+
+        (function test(){
+            if (fired) {
+                callback(true);
+                insertPoint.removeChild(link);
+            } else if (allow--) {
+                setTimeout(test, 0);
+            } else {
+                callback(false);
+                insertPoint.removeChild(link);
+            }
+        })();
+    }
+
+    function testComplete(){
+        waitTest = false;
+
+        // defines delayed first
+        while (delay.length) {
+            moduleDefine.apply(global, delay.shift());
         }
     }
 
-    function checker(/* var */id , link, sheet, rules, cross){
+    function delayExecute(func, param){
+        setTimeout(function(){
+            func(param);
+        });
+    }
+
+    function hasCssRule(sheet){
+        try {
+            sheet = sheet.sheet || sheet.styleSheet;
+            return 0 < (sheet.rules || sheet.cssRules).length;
+        } catch(_) {
+            // cross domain:
+            // IE: file unavailable.
+            return !isIE || false;
+        }
+    }
+
+    function checkComplete(/* var */id, link, sheet, rules, cross){
         for (id in queue) {
             cross = isWebkit;
             link = links[id];
-            // if file is loading, retrieves 'cssRules' property in firefox
-            // will throws an error.
+
+            // Webkit: creates link.sheet right after file loaded
             if (sheet = link.sheet) {
+                // Gecko: also throws error when loading
                 try { rules = sheet.cssRules; } catch(_) {
-                    // Gecko: onload - cross domain
-                    cross = _.code === _.SECURITY_ERR;
+                    // Opera/Gecko: onload - cross domain
+                    cross = isOpera || _.code === (_.SECURITY_ERR || 1000);
                 }
 
                 if (rules) {
                     link[rules.length ? eventOnload : eventOnfail]();
                 } else if (cross) {
-                    // Webkit/Gecko: onload - cross domain
                     link[eventOnload]();
                 }
             }
         }
-        setTimeout(checkQueue, 100);
+
+        // continue or stop
+        timer = id ? setTimeout(checkComplete, 100) : 0;
     }
 
 }(this, /*@cc_on!@*/!1));
